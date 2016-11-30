7d77fd4b339dfe6e539233392e85674ace543909
Adrian <adrian.lugo.bendezu@gmail.com>
2016-11-30 14:36:36 -0400

    Add files via upload

---
+++ Proyecto.py
@@ -0,0 +1,38 @@
+# un programa con base de datos de paises
+# guardaremos nombre, capital, poblacion, superficie, continente en el cual se encuentra...
+# lista de idiomas oficiales y lista de monedas oficiales
+
+lst = [[100, 'France', 'Paris', 60000000, 640000, 'Europe', ['French'], ['European euro']],
+       [101, 'Panama', 'Panama City', 4000000, 75000, 'Central America and Caribbean', ['Spanish'], ['Balboa', 'US Dollar']],
+       [102, 'Switzerland', 'Bern', 8300000, 40000, 'Europe', ['German', 'Italian', 'French', 'Romansh'], ['Swiss Franc']],
+       [103, 'United States', 'Washington DC', 320000000, 10000000, 'North America', ['English'], ['US Dollar']],
+       [104, 'Argentina', 'Buenos Aires', 44000000, 2700000, 'South America', ['Spanish'], ['Argentine Peso']],
+       [105, 'Burkina Faso', 'Ouagadougou', 19000000, 270000, 'Africa', ['French'], ['West African CFA franc']],
+       [106, 'Cote d\'ivoire', 'Yamoussoukro', 24000000, 320000, 'Africa', ['French'], ['West African CFA franc']],
+       [107, 'Belarus', 'Minsk', 9500000, 200000, 'Europe', ['Russian', 'Belarusian'], ['Belarusian ruble']],
+       [108, 'Iran', 'Tehran', 83000000, 1600000, 'Middle East', ['Persian'], ['Iranian rial']],
+       [109, 'Russia', 'Moscow', 140000000, 17000000, 'Central Asia', ['Russian'], ['Russian ruble']],
+       [110, 'India', 'New Delhi', 1200000000, 3300000, 'South Asia', ['English', 'Hindi', 'Bengali', 'Telugu', 'Marathi', 'Tamil', 'Urdu', 'Gujarati', 'Malayalam', 'Kannada', 'Oriya', 'Punjabi', 'Assamese', 'Kashmiri', 'Sindhi', 'Sanskrit'], ['Indian rupee']],
+       [111, 'China', 'Beijing', 1400000000, 9600000, 'East and Southeast Asia', ['Mandarin'], ['Chinese Yuan Renminbi']],
+       [112, 'Australia', 'Canberra', 23000000, 7700000, 'Australia-Oceania', ['English'], ['Australian dollar']],
+       [113, 'Colombia', 'Bogota', 47000000, 1100000, 'South America', ['Spanish'], ['Colombian peso']],
+       [114, 'Lithuania', 'Vilnius', 2800000, 65300, 'Europe', ['Lithuanian'], ['European euro']],
+       [115, 'Puerto Rico', 'San Juan', 3500000, 13800, 'Central America and Caribbean', ['Spanish', 'English'], ['US Dollar']],
+       [116, 'Mongolia', 'Ulaanbaatar', 3000000, 1500000, 'East and Southeast Asia', ['Khalkha Mongol'], ['Mongolian tugrik']],
+       [117, 'Burma', 'Rangoon', 57000000, 670000, 'East and Southeast Asia', ['Burmese'], ['Myanmar kyat']],
+       [118, 'Suriname', 'Paramaribo', 600000, 170000, 'South America', ['Dutch'], ['Surinamese dollar']],
+       [119, 'Japon', 'Tokyo', 130000000, 380000, 'East and Southeast Asia', ['Japanese'], ['Japanese yen']]]
+
+
+# sera un juego que haga preguntas sobre los paises
+# preguntas ejemplo: en que continente se encuentra la costa de marfil? dame el idioma oficial de argentina?...
+# cual es la poblacion de tal?
+# hay que darle puntos al usuario si acierta
+# quitarle puntos si no acierta???? preguntar
+# cuando harto el programa hay que proveer opcion para manejar base de datos o seguir jugando, dos grandes funciones
+# las 4 cosas que hacer con una base de datos: agregar, modificar, consultar(dame la info de francia)
+# burkina faso era otra cosa antes?
+# idea: poner opcion para saber que consultas se puede hacer
+# hacer un archivo de texto que contenga la info de los paises es una opcion
+# minimo 20 paises
+# fecha de entrega es 1 de diciembre
